b0c1060cd754dd85b504c775b890aa20c58ad809
koheinhtetaung <koheinhtetaung94@gmail.com>
2024-09-29 00:09:09 +0800

    modify 2.file

--- 2.create-aws-iam-user-tfc/main.tf
+++ 2.create-aws-iam-user-tfc/main.tf
@@ -37,7 +37,7 @@ data "aws_iam_policy_document" "inline_po_vault" {
         "iam:RemoveUserFromGroup"
         ]
     resources = [
-		"arn:aws:iam::905418241304:user/vault-*"
+		"arn:aws:iam::767397769779:user/vault-*"
         ]
   }
 }
